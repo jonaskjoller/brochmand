d77e0fadb2c991bfdfc176d41c4fbee8a9d227c9
Jonas Kjøller-Rasmussen <49672415+jonaskjoller@users.noreply.github.com>
2021-11-11 13:22:15 +0100

    Update and rename stopword_ss_winter_1p-10p.py to stopword_1p-10p.py

--- tools/stopword_1p-10p.py
+++ tools/stopword_1p-10p.py
@@ -1,11 +1,24 @@
 import re
+import os
 
 wordlist = []
 wordfreq = {}
+file_list = []
+counter = 0
 
-#filename = input('Enter filename: ')
-filename = 'Sabbati_Sanctificatio_Vinter_(Brochmand).txt'
+# List possible files in folder
+for items in os.listdir():
+    if items.endswith('.txt'):
+        file_list.append(items)
+        print (counter, items)
+        counter += 1
+        
+filename = file_list[int(input('Select file by entering file number: '))]
 
+#Get name of folder to use as naming scheme
+keyword = os.getcwd().split('\\')[-1]
+
+#Clean file
 with open(filename,'r',encoding='utf-8') as file:
     file = file.read()
     file = file.lower() #Lower case
@@ -14,18 +27,20 @@ with open(filename,'r',encoding='utf-8') as file:
     file = re.sub('\n',' ',file) #Substitute new line with space
     file = re.sub(' +', ' ', file) #Reduce surplus spacing
     wordlist = file.split()
-   
+
+#Build word frequency
 for w in wordlist:
     if w not in wordfreq:
         wordfreq[w] = 1
     else:
         wordfreq[w] += 1
 
+#Get position of top 1 percent and bottom 10 percent
 pos_top20 = int(len(wordfreq)*0.01)
 pos_low10 = int(len(wordfreq)*0.1)
 
-
-with open('stopword_ss_winter_1p-10p.txt','w',encoding='utf-8') as file:
+#Save a new file
+with open('stopword_' + keyword + '_1p-10p.txt','w',encoding='utf-8') as file:
     for k,v in sorted(wordfreq.items(),key=lambda item: item[1], reverse=True)[:pos_top20]:
         file.write(k+'\n')
     for k,v in sorted(wordfreq.items(),key=lambda item: item[1], reverse=False)[:pos_low10]:
