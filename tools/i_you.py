c60e602e55e6e420adcfc1bf70affed1424328f0
Jonas Kjøller-Rasmussen <49672415+jonaskjoller@users.noreply.github.com>
2021-12-02 14:31:07 +0100

    Update i_you.py

--- tools/i_you.py
+++ tools/i_you.py
@@ -14,34 +14,37 @@ for doc in os.listdir(foldername):
     if doc.endswith('.txt'):
         doc_list.append(foldername + '/' + doc)
 
-for doc in doc_list[5:6]:  
+for doc in doc_list[43:44]:  
     with open(doc,'r',encoding='utf-8') as file:
         freq_section = []
         freq = ()
         file = file.read().split()
         file_section = []
+        #Split file into smaller sections
         for i in range(0, len(file), 150):
             file_section.append(file[i:i + 150])
+        #Count frequence of 1st and 2nd person pronouns in section
         for section in file_section:
             counter_i = 0
             counter_you = 0
-            for word in section:
-                counter = 0
-                if word in list_i:
+            for w in range(0,len(section)):
+                if section[w] in list_i:
                     counter_i+= 1             
-                elif word in list_you:
-                    if word == 'i':
-                        if str(file[counter+1]+' '+file[counter+2]) in filter_you:
+                elif section[w] in list_you:
+                    if section[w] == 'i':
+                        if str(file[w+1]+' '+file[w+2]) in filter_you:
                             counter_you += 1
                     else:
                         counter_you += 1   
-                counter += 1
-            freq = (counter_you,counter_i)
-            freq_section.append(freq)
-            if counter_i > counter_you:
-                print(freq,':',end=' ')
-                for passage in file_section[file_section.index(section)-1:file_section.index(section)+2]:
-                    for word in passage:
-                        print(word,end=' ')
-                print('\n')
+            # freq = (counter_you,counter_i)
+            # freq_section.append(freq)
+            freq_section.append(counter_you-counter_i)
+        for x in range(0,len(file_section)):
+            if freq_section[x] < 0:
+                if freq_section[x-1] >= 0:
+                    print(freq_section[x],':',end=' ')
+                for word in file_section[x]:
+                    print(word,end=' ')
+                if freq_section[x+1] >= 0:
+                    print('\n')
         print(freq_section)
